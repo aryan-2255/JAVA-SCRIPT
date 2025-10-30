ef030c6cc4b1b3295ff7d23b9c26dc38e86ff811
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2025-10-30 22:05:46 +0530

    update

---
+++ B11.)  Logical Operator/script_11.js
@@ -0,0 +1,14 @@
+// Logical operator = use to combine or manuplate boolean values (true or false)
+
+// and = &&
+// or = ||
+// not = !
+
+
+const temp = 25;
+if(temp>0 && temp <= 30 ){
+    console.log("the weather is good")
+}
+else{
+    console.log("the weather is bad")
+}
