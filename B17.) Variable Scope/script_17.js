c7e1383b887d86e5d73c962f0093f9315538f9f1
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2025-11-01 04:36:09 +0530

    update

--- B17.) Variable Scope/script_17.js
+++ B17.) Variable Scope/script_17.js
@@ -2,3 +2,15 @@
 //                  and accessible (local v/s global)
 
 
+let x = 1;
+// let x = 2    # can not
+
+function function1() {
+let x = 1;
+console.log(y);
+}
+
+function function2(){
+let y = 2;
+console.log(x) ;
+}
